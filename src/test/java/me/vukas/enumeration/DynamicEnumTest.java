77a83654a06ee9bb869706b51c60e274b033e2f3
wanghehan <wanghehan@xiaomi.com>
2022-02-21 13:00:53 +0800

    dynamic enum

--- src/test/java/me/vukas/enumeration/DynamicEnumTest.java
+++ src/test/java/me/vukas/enumeration/DynamicEnumTest.java
@@ -1,72 +1,37 @@
 package me.vukas.enumeration;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 public class DynamicEnumTest {
     @Test
-    public void DemoEnumUsage1() {
-        System.out.println("==DEMO ENUM TEST1 ==");
-        System.out.println(Arrays.toString(DynamicEnum.values()));
-        DynamicEnum.add("FOUR", 4, "444");
-        System.out.println(Enum.valueOf(DynamicEnum.class, "FOUR").getAlternativeName());
-        System.out.println(Arrays.toString(DynamicEnum.values()));
-        DynamicEnum.remove("FOUR");
-        System.out.println(Enum.valueOf(DynamicEnum.class, "FOUR").getAlternativeName());
-        System.out.println(Arrays.toString(DynamicEnum.values()));
-        DynamicEnum.update("ONE", -1, "-111");
-        System.out.println(Enum.valueOf(DynamicEnum.class, "ONE").getAlternativeName());
-        System.out.println(DynamicEnum.ONE.getId());
-        System.out.println(DynamicEnum.values()[0].getAlternativeName());
-    }
-
-    @Test
     public void DemoEnumUsage() {
         System.out.println("==DEMO ENUM TEST ==");
-        for (DynamicEnum dynamicEnum : DynamicEnum.values()) {
-            System.out.println("Name: " + dynamicEnum.name() + "; Id:" + dynamicEnum.getId() + "; AlternativeName: " + dynamicEnum.getAlternativeName() + "; Ordinal: " + dynamicEnum.ordinal());
-        }
-
-        System.out.println("Adding 4,5,6 in runtime...");
-
-//        DynamicEnum.add("FOUR", 4);
-        DynamicEnum.add("FIVE", 5, "five");
-        DynamicEnum.add("SIX", 6, "six");
-
-        System.out.println("Values after update:");
-        for (DynamicEnum dynamicEnum : DynamicEnum.values()) {
-            System.out.println("Name: " + dynamicEnum.name() + "; Id:" + dynamicEnum.getId() + "; AlternativeName: " + dynamicEnum.getAlternativeName() + "; Ordinal: " + dynamicEnum.ordinal());
-        }
-
-        System.out.println("Deleting 5...");
-
-        DynamicEnum.remove("FIVE");
-
-        System.out.println("Values after removal of enum 5 (Six got new ordinal):");
-        for (DynamicEnum dynamicEnum : DynamicEnum.values()) {
-            System.out.println("Name: " + dynamicEnum.name() + "; Id:" + dynamicEnum.getId() + "; AlternativeName: " + dynamicEnum.getAlternativeName() + "; Ordinal: " + dynamicEnum.ordinal());
-        }
-
-        System.out.println("Readding 5 and adding 10...");
-
-//        DynamicEnum.add("FIVE", 5);
-        DynamicEnum.add("TEN", 10, "ten");
-
-        System.out.println("Values after update:");
-        for (DynamicEnum dynamicEnum : DynamicEnum.values()) {
-            System.out.println("Name: " + dynamicEnum.name() + "; Id:" + dynamicEnum.getId() + "; AlternativeName: " + dynamicEnum.getAlternativeName() + "; Ordinal: " + dynamicEnum.ordinal());
-        }
-
-        System.out.println("Trying to remove 1 (cannot because it is hardcoded) and remove 6...");
-
-        DynamicEnum.remove("ONE");
-        DynamicEnum.remove("SIX");
-
-        System.out.println("Values after update:");
-        for (DynamicEnum dynamicEnum : DynamicEnum.values()) {
-            System.out.println("Name: " + dynamicEnum.name() + "; Id:" + dynamicEnum.getId() + "; AlternativeName: " + dynamicEnum.getAlternativeName() + "; Ordinal: " + dynamicEnum.ordinal());
-        }
+        Assert.assertEquals("[ONE, TWO, THREE]", Arrays.toString(DynamicEnum.values()));
+        //add
+        DynamicEnum.add("FOUR", 4, "444");
+        Assert.assertEquals("444", Enum.valueOf(DynamicEnum.class, "FOUR").getAlternativeName());
+        Assert.assertEquals("444", DynamicEnum.valueOf("FOUR").getAlternativeName());
+        Assert.assertEquals("[ONE, TWO, THREE, FOUR]", Arrays.toString(DynamicEnum.values()));
+        boolean addDup = DynamicEnum.add("FOUR", 14324, "xx");
+        Assert.assertFalse(addDup);
+        //remove
+        DynamicEnum.remove("FOUR");
+        Assert.assertEquals("[ONE, TWO, THREE]", Arrays.toString(DynamicEnum.values()));
+        boolean removeNosExist = DynamicEnum.remove("FOUR");
+        Assert.assertFalse(removeNosExist);
+        boolean removeOrigin = DynamicEnum.remove("ONE");
+        Assert.assertFalse(removeOrigin);
+        //update
+        boolean updateNotExist = DynamicEnum.update("FIVE", 1313, "adaff");
+        Assert.assertFalse(updateNotExist);
+        DynamicEnum.update("ONE", 1, "updated one");
+        Assert.assertEquals("updated one", DynamicEnum.valueOf("ONE").getAlternativeName());
+        Assert.assertEquals("updated one", Enum.valueOf(DynamicEnum.class, "ONE").getAlternativeName());
+        Assert.assertEquals("updated one", DynamicEnum.values()[0].getAlternativeName());
+        System.out.println("==DEMO ENUM TEST END ==");
     }
 
 }
